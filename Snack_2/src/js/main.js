f72add70c544cc267d973af250da5f7f51ad55c0
DarioBosco <dario_bosco@protonmail.com>
2021-08-31 15:27:18 +0200

    Creato l'array delle squadre

---
+++ Snack_2/src/js/main.js
@@ -0,0 +1,18 @@
+//Creare un array di oggetti di squadre di calcio. Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+//Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+//Generare numeri random al posto degli 0 nelle proprietà:
+//Punti fatti e falli subiti.
+//Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+
+//Creare un array di oggetti di squadre di calcio. Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+//Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+const teams = [
+	{ name: 'Team 1', InflictedPenalties: 0, sufferedPenalties: 0 },
+	{ name: 'Team 2', InflictedPenalties: 0, sufferedPenalties: 0 },
+	{ name: 'Team 3', InflictedPenalties: 0, sufferedPenalties: 0 },
+	{ name: 'Team 4', InflictedPenalties: 0, sufferedPenalties: 0 },
+	{ name: 'Team 5', InflictedPenalties: 0, sufferedPenalties: 0 },
+	{ name: 'Team 6', InflictedPenalties: 0, sufferedPenalties: 0 },
+	{ name: 'Team 7', InflictedPenalties: 0, sufferedPenalties: 0 },
+	{ name: 'Team 8', InflictedPenalties: 0, sufferedPenalties: 0 },
+];
