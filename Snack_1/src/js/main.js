419dd0fe230d8251a34cc24d4c37475f6b615161
DarioBosco <dario_bosco@protonmail.com>
2021-08-31 14:12:36 +0200

    Creato l'array di oggetti

---
+++ Snack_1/src/js/main.js
@@ -0,0 +1,24 @@
+/* 
+Creare un array di oggetti:
+Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+Stampare a schermo la bici con peso minore utilizzando destructuring e template literal
+BONUS: inserire una arrow function che preso in input l'array di bici ritorni l'oggetto con bici più leggera
+*/
+
+//Creare un array di oggetti:
+//Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+
+const bikes = [
+	{ name: 'Nome 1', weight: 12 },
+	{ name: 'Nome 2', weight: 14 },
+	{ name: 'Nome 3', weight: 7 },
+	{ name: 'Nome 4', weight: 24 },
+	{ name: 'Nome 5', weight: 14 },
+	{ name: 'Nome 6', weight: 7 },
+	{ name: 'Nome 7', weight: 15 },
+	{ name: 'Nome 8', weight: 22 },
+	{ name: 'Nome 9', weight: 13 },
+	{ name: 'Nome 10', weight: 8.3 },
+];
+
+//Stampare a schermo la bici con peso minore utilizzando destructuring e template literal
