8c236c038c5b931995edf5dcb452470a7da02ac6
DarioBosco <dario_bosco@protonmail.com>
2021-09-01 09:55:30 +0200

    Modificato inflictedPenalties in scores

--- Snack_2/src/js/main.js
+++ Snack_2/src/js/main.js
@@ -1,18 +1,18 @@
 //Creare un array di oggetti di squadre di calcio. Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
 //Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
 const teams = [
-	{ name: 'Team 1', inflictedPenalties: 0, sufferedPenalties: 0 },
-	{ name: 'Team 2', inflictedPenalties: 0, sufferedPenalties: 0 },
-	{ name: 'Team 3', inflictedPenalties: 0, sufferedPenalties: 0 },
-	{ name: 'Team 4', inflictedPenalties: 0, sufferedPenalties: 0 },
-	{ name: 'Team 5', inflictedPenalties: 0, sufferedPenalties: 0 },
-	{ name: 'Team 6', inflictedPenalties: 0, sufferedPenalties: 0 },
-	{ name: 'Team 7', inflictedPenalties: 0, sufferedPenalties: 0 },
-	{ name: 'Team 8', inflictedPenalties: 0, sufferedPenalties: 0 },
+	{ name: 'Team 1', scores: 0, sufferedPenalties: 0 },
+	{ name: 'Team 2', scores: 0, sufferedPenalties: 0 },
+	{ name: 'Team 3', scores: 0, sufferedPenalties: 0 },
+	{ name: 'Team 4', scores: 0, sufferedPenalties: 0 },
+	{ name: 'Team 5', scores: 0, sufferedPenalties: 0 },
+	{ name: 'Team 6', scores: 0, sufferedPenalties: 0 },
+	{ name: 'Team 7', scores: 0, sufferedPenalties: 0 },
+	{ name: 'Team 8', scores: 0, sufferedPenalties: 0 },
 ];
 
 //Generare numeri random al posto degli 0 nelle proprietà: punti fatti e falli subiti
-const inflictedPenaltiesByTeam = [];
+const insights = [];
 
 for (let i = 0; i < teams.length; i++) {
 	const element = teams[i];
@@ -20,15 +20,15 @@ for (let i = 0; i < teams.length; i++) {
 	const randomInt = (min, max) => {
 		return Math.floor(Math.random() * (max - min + 1) + min);
 	};
-	//Assegno i numeri random alle proprietà inflictedPenalties e sufferedPenalties
-	element.inflictedPenalties = randomInt(1, 10);
+	//Assegno i numeri random alle proprietà scores e sufferedPenalties
+	element.scores = randomInt(1, 10);
 	element.sufferedPenalties = randomInt(1, 10);
 	//Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
-	const { name, inflictedPenalties } = element;
-	inflictedPenaltiesByTeam.push({
+	const { name, scores } = element;
+	insights.push({
 		name: name,
-		inflictedPenalties: inflictedPenalties,
+		scores: scores,
 	});
 }
 
-console.log(inflictedPenaltiesByTeam);
+console.log(insights);
